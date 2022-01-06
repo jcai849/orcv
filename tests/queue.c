b6c591a8c00c624dd4fa4a4ed7ac434c6fa31d78
Jason Cairns <jason.cairns@aucklanduni.ac.nz>
2022-01-07 11:34:59 +1300

    seperate asymmetric tests in excess push and pull

--- tests/queue.c
+++ tests/queue.c
@@ -14,36 +14,48 @@ int test_unused(void)
 int test_symmetric_use(void)
 {
     Queue *symmetric_queue;
-    int item;
+    int item = 1;
     int *popped;
 
     symmetric_queue = make_queue();
-    item = 1;
     push_queue(symmetric_queue, &item);
     queue_empty(symmetric_queue);
     popped = pop_queue(symmetric_queue);
-    assert(*popped == 1);
+    assert(*popped == item);
     rm_queue(symmetric_queue);
     return 0;
 }
 
-int test_asymmetric_use()
+int test_excess_push()
 {
-    Queue *asymmetric_queue;
-    int items[] = { 1, 2, 3, 4};
+    Queue *pusher_queue;
+    int items[] = {1, 2};
+    int *popped;
+
+    pusher_queue = make_queue();
+    push_queue(pusher_queue, &items[0]);
+    push_queue(pusher_queue, &items[1]);
+    assert(!queue_empty(pusher_queue));
+    popped = pop_queue(pusher_queue);
+    assert(*popped = items[0]);
+    rm_queue(pusher_queue);
+    return 0;
+}
+
+int test_excess_pop()
+{
+    Queue *popper_queue;
+    int item = 1;
     int *popped[2];
 
-    asymmetric_queue = make_queue();
-    push_queue(asymmetric_queue, &items[0]);
-    push_queue(asymmetric_queue, &items[1]);
-    assert(!queue_empty(asymmetric_queue));
-    popped[0] = pop_queue(asymmetric_queue);
-    assert(*popped[0] == items[0]);
-    popped[1] = pop_queue(asymmetric_queue);
-    assert(*popped[1] == items[1]);
-    push_queue(asymmetric_queue, &items[2]);
-    push_queue(asymmetric_queue, &items[3]);
-    rm_queue(asymmetric_queue);
+    popper_queue = make_queue();
+    push_queue(popper_queue, &item);
+    assert(!queue_empty(popper_queue));
+    popped[0] = pop_queue(popper_queue);
+    assert(*popped[0] == item);
+    popped[1] = pop_queue(popper_queue);
+    assert(popped[1] == NULL);
+    rm_queue(popper_queue);
     return 0;
 }
 
@@ -51,6 +63,7 @@ int main(void)
 {
     test_unused();
     test_symmetric_use();
-    test_asymmetric_use();
+    test_excess_push();
+    test_excess_pop();
     return 0;
 }
