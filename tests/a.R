41cf5bf3af3f21a2768fa04df7a4b0b78c405c73
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-06-30 12:24:34 +1200

    skeleton test structure

--- tests/a.R
+++ tests/a.R
@@ -1,6 +1,23 @@
 library(orcv)
+INADDR_LOOPBACK <- strtoi("7f000001", 16)
 start("localhost", 9797L)
 location()
-msg <- receive()
-send(fd(msg), "hello to you too", 10:1)
+other <- as.Location(c(INADDR_LOOPBACK, 9898L))
 
+## INIT
+
+# 1) send to character address
+# 2) receive loc from queue
+
+## X 1
+
+# 1) send to location after other receive, keep_conn a
+# 2) send to location after other receive
+# 3) send to location before receive, keep_conn b
+# 4) send to location before receive
+# 5) send to fd a after other receive, keep_conn a
+# 6) send to fd a after other receive
+# 7) send to fd b before other receive, keep_conn b
+# 8) send to fd b before other receive, keep_conn b
+# 9) send to fd b when closed by other
+#10) send to fd a when closed by self
--- tests/b.R
+++ tests/b.R
@@ -1,9 +1,23 @@
 library(orcv)
+INADDR_LOOPBACK <- strtoi("7f000001", 16)
 start("localhost", 9898L)
 location()
-
-INADDR_LOOPBACK <- strtoi("7f000001", 16)
 other <- as.Location(c(INADDR_LOOPBACK, 9797L))
-fd <- send(other, "hello", 1:10)
-receive(fd)
 
+## INIT
+
+# 1) receive loc from queue
+# 2) send self_loc to loc
+
+## X 1
+
+# 1) receive from queue before other send, keep_conn a
+# 2) receive from queue before other send
+# 3) receive from queue after other send, keep_conn b
+# 4) receive from queue after other send
+# 5) receive from fd a before other receive, keep_conn a
+# 6) receive from fd a before other receive, keep_conn a
+# 7) receive from fd b after other receive, keep_conn b
+# 8) receive from fd b after other receive
+# 9) receive from fd a when closed by other
+#10) receive from fd b when closed by self
