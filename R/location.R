2570bc3cf08b6a4f132e6cd66a6e353f0a790e4e
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-07-05 18:12:48 +1200

    create location class

--- R/location.R
+++ R/location.R
@@ -3,12 +3,16 @@ port <- function(x, ...) UseMethod("port", x)
 location <- function(x, ...) if (missing(x)) as.Location(.Call(C_location)) else UseMethod("location", x)
 as.Location <- function(x, ...) UseMethod("as.Location", x)
 
+location.numeric <- function(x, ...) location(as.integer(x))
 location.integer <- function(x, ...) {
 	if (x < 0) stop("invalid 'length' argument")
-	if (x == 0) return(structure(list(), class="Location"))
-	lapply(0:x, function(i) as.Location(integer(1), integer(1)))
+	if (x == 0) return(as.Location(list()))
+	rep(as.Location(integer(2)), x)
 }
 
+as.Location.list <- function(x, ...) {
+	structure(x, class="Location")
+}
 as.Location.character <- function(x, port, ...) {
         stopifnot(is.integer(port))
         as.Location(.Call(C_loc_from_string, x, port))
@@ -16,24 +20,57 @@ as.Location.character <- function(x, port, ...) {
 as.Location.integer <- function(x, port, ...) {
         if (!missing(port)) x <- c(x, port)
         names(x) <- c("address", "port")
-	loc <- list(x)
-        class(loc) <- "Location"
-       	loc 
+	as.Location(list(x))
+}
+is.Location <- function(x, ...) {
+	inherits(x, "Location")
 }
 
-address.Location <- function(x, ...) sapply(x, "[[", 1)
-port.Location <- function(x, ...) sapply(x, "[[", 2)
+rep.Location <- function(x, times) {
+	as.Location(rep(unclass(x), times))
+}
+c.Location <- function(...) {
+	as.Location(c(sapply(list(...), unclass)))
+}
+as.data.frame.Location <- function(x, row.names=NULL, optional=FALSE, ...) {
+	nm <- deparse1(substitute(x))
+	df <- list2DF(list(unclass(x)))
+	if (optional)
+		names(df) <- nm
+	if (!is.null(row.names))
+		attr(df, "row.names") <- row.names
+        df
+}
 
-format.Location
-print.Location
+as.list.Location <- function(x, ...) {
+	lapply(unclass(x), as.Location)
+}
+`[.Location` <- function(x, i) {
+	as.Location(unclass(x)[i])
+}
+`[<-.Location` <- function(x, i, value) {
+	stopifnot(is.Location(value))
+	x <- unclass(x)
+	x[i] <- value
+	as.Location(x)
+}
 
-as.data.frame.Location
-c.Location
+address.Location <- function(x, ...) vapply(unclass(x), "[[", integer(1), 1)
+port.Location <- function(x, ...) vapply(unclass(x), "[[", integer(1), 2)
 
-as.list.Location
-`[.Location`
+format.Location <- function(x, ...) {
+	format(data.frame(address=address(x), port=port(x)))
+}
+print.Location <- function(x, ...) {
+	cat("Location:\n")
+	print(format(x))
+}
 
-Ops.Location
+Ops.Location <- function(e1, e2) {
+	stopifnot(is.Location(e2))
+	do.call(.Generic, address(e1), address(e2)) &
+		do.call(.Generic, port(e1), port(e2))
+}
 
 send.Location <- function(x, header, payload=NULL, keep_conn=FALSE, ...) {
         fd <- as.FD(mapply(function(a, p) .Call(C_get_socket, a, p),  address(x), port(x)))
